7995920baf68286886b8582a529980d1fcea9114
jeromedecoster <github@jeromedecoster.com>
2016-07-04 16:20:21 +0200

    0.2.1

--- only.js
+++ only.js
@@ -1,6 +1,9 @@
+const isObject = require('is-funcs/is-object')
+
 const has = Object.prototype.hasOwnProperty
 
 module.exports = function(obj, keys) {
+  if (!isObject(obj)) return {}
   if (typeof keys === 'string') keys = keys.trim().split(/ +/)
   else if (Array.isArray(keys) === false) return {}
   return keys.reduce(function(prev, curr) {
